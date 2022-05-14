9a6680790e68d26dd31c3558b6eb25ea6739696b
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-05-14 22:27:17 +0900

    いらないコードを削除

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div id="app">
     <div class="cell cell-map">
-      <OlMap></OlMap>
+      <OlMap />
     </div>
-    <!-- <div class="cell cell-edit">
-      Edit
-    </div>
-    <div class="cell cell-inspect">
-      Inspect
-    </div> -->
   </div>
 </template>
 
-<script>
+<script lang="ts">
   import OlMap from './components/OlMap.vue';
   export default {
     name: 'App',
@@ -23,40 +17,5 @@
 </script>
 
 <style>
-  html, body {
-    height: 100%;
-    margin: 0;
-  }
 
-  #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    height: 100%;
-    display: grid;
-    grid-template-columns: 100vh;
-    grid-auto-rows: 1fr;
-    grid-gap: 1rem;
-    padding: 1rem;
-    box-sizing: border-box;
-  }
-
-  .cell {
-    border-radius: 4px;
-    background-color: lightgrey;
-  }
-
-  .cell-map {
-    grid-column: 1;
-    grid-row-start: 1;
-    grid-row-end: 3;
-  }
-
-  .cell-edit {
-    grid-column: 2;
-    grid-row: 1;
-  }
-
-  .cell-inspect {
-    grid-column: 2;
-    grid-row: 2;
-  }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    }
+  },
+  server: {
+    proxy: {
+      '/api/': {
+        target: 'http://localhost:3001'
+    }
+  }}
 })
