c144b5843b7353fe34d0574115f9bcf7301f9214
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-05-14 22:28:50 +0900

    チェックボックスで六角川レイヤーを切り替え表示する

--- src/components/OlMap.vue
+++ src/components/OlMap.vue
@@ -1,32 +1,65 @@
 <template>
-    <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:100%">
+    <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:800px">
 
-        <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" 
-        :projection="projection" />
-
+        <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" :projection="projection" />
         <ol-tile-layer>
             <ol-source-osm />
         </ol-tile-layer>
-        
+
+        <div id="v-model-checkbox" class="demo">
+            <input type="checkbox" id="checkbox" v-model="checked" />
+            <label for="checkbox">六角川</label>
+            <p v-if="checked">
+                <Rokkakugawa />
+            </p>
+        </div>
     </ol-map>
 </template>
 
-<script>
-    import {ref} from 'vue'
+<script lang="ts">
+import {
+    ref, inject
+} from 'vue'
+import Rokkakugawa from './rokkakugawa.vue'
+export default {
+    setup() {
+        const center = ref([130.094129, 33.196412]);
+        const projection = ref("EPSG:4326");
+        const zoom = ref(12);
+        const rotation = ref(0);
+        const rokkakugawa = ref("src/data/六角川_20210815.geojson");
+        const format = inject("ol-format");
+        const geoJson = new format.GeoJSON();
+        const toggle = false;
 
-    export default {
-        setup() {
-            const center = ref([40, 40])
-            const projection = ref('EPSG:4326')
-            const zoom = ref(8)
-            const rotation = ref(0)
-            return {
-                center,
-                projection,
-                zoom,
-                rotation
-            }
-        },
-    }
+        return {
+            center,
+            projection,
+            zoom,
+            rotation,
+            rokkakugawa,
+            geoJson,
+            toggle
+        };
+    },
+    data() {
+        return {
+            checked: true
+        };
+    },
+    components: { Rokkakugawa }
+}
 </script>
 
+<style scoped>
+.demo {
+    font-family: sans-serif;
+    border: 1px solid #eee;
+    border-radius: 2px;
+    padding: 20px 30px;
+    margin-top: 1em;
+    margin-bottom: 40px;
+    user-select: none;
+    overflow-x: auto;
+}
+</style>
---
+++ src/components/rokkakugawa.vue
@@ -0,0 +1,57 @@
+<template>
+    <ol-vector-layer>
+        <ol-source-vector :url="rokkakugawa" :format="geoJson">
+        </ol-source-vector>
+        <ol-style>
+            <ol-style-stroke color="blue" :width="2"></ol-style-stroke>
+            <ol-style-fill color="rgba(0,255,255,0.1)"></ol-style-fill>
+        </ol-style>
+    </ol-vector-layer>
+</template>
+
+<script lang="ts">
+import {
+    ref, inject
+} from 'vue'
+export default {
+    setup() {
+        const center = ref([130.094129, 33.196412])
+        const projection = ref('EPSG:4326')
+        const zoom = ref(12)
+        const rotation = ref(0)
+        const rokkakugawa = ref('src/data/六角川_20210815.geojson')
+        const format = inject('ol-format');
+        const geoJson = new format.GeoJSON();
+        const toggle = false
+
+        return {
+            center,
+            projection,
+            zoom,
+            rotation,
+            rokkakugawa,
+            geoJson,
+            toggle
+        }
+    },
+
+    data() {
+        return {
+            checked: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.demo {
+    font-family: sans-serif;
+    border: 1px solid #eee;
+    border-radius: 2px;
+    padding: 20px 30px;
+    margin-top: 1em;
+    margin-bottom: 40px;
+    user-select: none;
+    overflow-x: auto;
+}
+</style>
