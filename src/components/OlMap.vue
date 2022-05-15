22494a4d7e697837d4419e29bcc0508a1e7dfc8a
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-05-15 18:41:40 +0900

    チェックボックスでレイヤ表示の切り替えはv-modelだけでいけるっぽい

--- src/components/OlMap.vue
+++ src/components/OlMap.vue
@@ -2,21 +2,23 @@
     <ol-map :loadTilesWhileAnimating="true" :loadTilesWhileInteracting="true" style="height:800px">
 
         <ol-view ref="view" :center="center" :rotation="rotation" :zoom="zoom" :projection="projection" />
+        <ol-fullscreen-control />
+        <ol-mouseposition-control />
         <ol-tile-layer>
             <ol-source-osm />
         </ol-tile-layer>
-        <li class="layerCheckbox">
+        <li class="layerPanel">
             <ul>
-                <input type="checkbox" id="checkbox-rokkakugawa" v-model="checkedR" />
+                <input type="checkbox" id="checkbox-rokkakugawa" v-model="switchrokkakugawa" />
                 <label for="checkbox">令和3年(2021年)8月 六角川 浸水想定図</label>
-                <p v-if="checkedR">
+                <p v-if="switchrokkakugawa">
                     <Rokkakugawa />
                 </p>
             </ul>
             <ul>
-                <input type="checkbox" id="checkbox-atami" v-model="checkedA" />
+                <input type="checkbox" id="checkbox-atami" v-model="switchatami" @change="switchCenter" />
                 <label for="checkbox">令和3年(2021年)7月1日 熱海地区 崩壊地当分布図</label>
-                <p v-if="checkedA">
+                <p v-if="switchatami">
                     <Atami />
                 </p>
             </ul>
@@ -33,29 +35,37 @@ import Atami from './atami.vue'
 export default {
     setup() {
         const center = ref([130.094129, 33.196412]);
+        // const center = ref([139.063595, 35.096493]);
         const projection = ref("EPSG:4326");
         const zoom = ref(12);
         const rotation = ref(0);
-        const toggle = false;
-        const checkedR = ref(true)
-        const checkedA = ref(true)
+        const switchCenter = (e) => {
+            if (e.target.checked) {
+                center.value = [139.063595, 35.096493]
+            }
+        }
+
 
         return {
             center,
             projection,
             zoom,
             rotation,
-            toggle,
-            checkedR,
-            checkedA
+            switchCenter,
         };
+    },
+    data() {
+        return{
+            switchatami: false,
+            switchrokkakugawa: false,
+        }
     },
     components: { Rokkakugawa, Atami }
 }
 </script>
 
 <style scoped>
-.layerCheckbox {
+.layerPanel {
     font-family: sans-serif;
     border: 1px solid #eee;
     border-radius: 2px;
